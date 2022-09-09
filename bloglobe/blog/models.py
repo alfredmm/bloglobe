f164674953e2d497133ef20c4cf2197809c3c092
alfredmm <alfredmutua2030@gmail.com>
2022-09-09 13:20:00 +0300

    Second commit- Added Commenting Section per Blog Post

--- bloglobe/blog/models.py
+++ bloglobe/blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
             return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete= models.CASCADE, related_name= "comments")
+    post = models.ForeignKey(Post, on_delete= models.CASCADE, related_name= 'comments')
     name = models.CharField(max_length=200)
     email = models.EmailField()
     body = models.TextField()
--- bloglobe/blog/views.py
+++ bloglobe/blog/views.py
@@ -1,8 +1,7 @@
-import imp
-from pdb import post_mortem
-from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -13,3 +12,32 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active= True)
+    new_comment = None
+
+
+    #Comment Posted
+   
+    if request.method == 'POST':
+        comment_form = CommentForm(data = request.POST)
+
+        if comment_form.is_valid():
+            #create the comment object but don't store it to database yet
+            new_comment = comment_form.save(commit=False)
+
+            #Assign the current post to comment
+            new_comment.post = post
+
+            #Save to Database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post':post,
+                                            'comments': comments,
+                                            'new_comment': new_comment,
+                                            'comment_form': comment_form})
